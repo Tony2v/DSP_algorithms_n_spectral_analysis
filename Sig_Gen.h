c691eec077cf25743842f66c56aa6e061537f722
Tony2v <42381507+Tony2v@users.noreply.github.com>
2018-10-22 14:17:24 +0300

    Add files via upload

--- Sig_Gen.h
+++ Sig_Gen.h
@@ -22,100 +22,96 @@ using af::Pi;
 
 class Sig_Generator
 {
-private:
-	SigGenPar prm;
-
 public:
 	Sig_Generator(const SigGenPar& sp) : prm(sp) {};
-	~Sig_Generator() {};
+	virtual ~Sig_Generator() {};
 
-	const af::array Get(const double& Tr) const;
+	virtual const af::array Get(double Tr, double Vr);
 
+protected:
+	void get_the_strobe(double Tr, double k);
+
+	SigGenPar prm;	
+	double Beg_pad;		// number of zeros to pad the beginning
+	double End_pad;		// number of zeros to pad the end
+	double T_shift;		// time shifting of the phase for the signal to be generated
+	double T_input;		// rec signal duration
+	
+	af::array Arg_phi;	// phase of the signal to be generated with paramters prm and Tr and k = (1-Vr/c)/(1+Vr/c)
 };
 
-const af::array Sig_Generator::Get(const double& Tr) const
+void Sig_Generator::get_the_strobe(double Tr, double k)
 {
-	if (prm.Sig == SigType::LFM)
+	if (Tr < prm.Ti + prm.Blank)
+	{
+		T_input = k*(Tr - prm.Blank);		// rec signal duration
+
+		T_shift = prm.Ti + prm.Blank - Tr;	// time shift for the rec signal phase		
+
+		// Padding the input sequence with End_pad zeros to get the real input:
+		End_pad = ceil(((prm.Q - 1)*prm.Ti - T_input)*prm.Fs);
+	}
+	else if (Tr < (prm.Q - k)*prm.Ti + prm.Blank)
+	{
+		T_input = k*prm.Ti;			// time duration of the received input signal
+
+		T_shift = 0;
+
+		// Padding the signal with zeros at the beginning and the end:
+		Beg_pad = floor((Tr - prm.Ti - prm.Blank)*prm.Fs);
+		End_pad = floor((prm.Q*prm.Ti + prm.Blank - Tr - T_input)*prm.Fs);
+	}
+	else
+	{
+		T_input = prm.Q*prm.Ti + prm.Blank - Tr;	// rec signal duration
+		T_shift = 0;
+		Beg_pad = floor((Tr - prm.Ti - prm.Blank)*prm.Fs);
+	}
+}
+
+
+const af::array Sig_Generator::Get(double Tr, double Vr)
+{	
+	double k = (1 - Vr / c_light) / (1 + Vr / c_light); // коэффициент трасформации временного масштаба сигнала из-за Допплера
+	
+	get_the_strobe(Tr, k);
+	
+	af::array ZZbeg = af::constant(0., Beg_pad, c64);
+	af::array ZZend = af::constant(0., End_pad, c64);
+	
+	double N = floor(T_input*prm.Fs);			// number of signal samples
+	af::array t(af::seq(N) / prm.Fs);			// creates an array of t [0:N-1]/fdis
+	
+	switch (prm.Sig)
+	{
+	case SigType::LFM:
 	{
 		double mu = prm.Fdev / prm.Ti;	// modulation rate parameter for the LFM
 
-		if (Tr < prm.Ti + prm.Blank)
-		{
-			double T_input = Tr - prm.Blank;			// rec signal duration
-			double N = floor(T_input*prm.Fs);			// number of signal samples
-			af::array t(af::seq(N) / prm.Fs);			// creates an array of t [0:N-1]/fdis
-			double T_shift = prm.Ti + prm.Blank - Tr;	// time shift for the rec signal phase
-
-			// Phase of the rec signal:
-			af::array Arg_phi = af::array(2.0*Pi*(prm.f0*(t + T_shift) + mu*pow((t + T_shift), 2.0) / 2.0)).as(f64);
-
-			af::array Samp_Re = af::cos(Arg_phi);   // generation of the real part of the signal with 1 magnitude!
-			af::array Samp_Im = af::sin(Arg_phi);	// generation of the image part of the signal with 1 magnitude!
-
-			af::array S = af::complex(Samp_Re, Samp_Im);	//  creates complex LFM signal
-
-			// Padding the input sequence with End_pad zeros to get the real input:
-			double End_pad = ceil(((prm.Q - 1)*prm.Ti - T_input)*prm.Fs);
-			
-			af::array ZZend = af::constant(0., End_pad, c64);
-
-			S = af::join(0, S, ZZend);
-
-			return S;
-		}
-		else if (Tr < (prm.Q - 1)*prm.Ti + prm.Blank)
-		{
-			double N = floor(prm.Ti*prm.Fs);				// number of signal samples
-
-			af::array t(af::seq(N) / prm.Fs);	// creates an array of t [0:N-1]/fdis
-
-												// Phase of the input signal:
-			af::array Arg_phi = af::array(2.0*Pi*(prm.f0*t + mu*pow(t, 2.0) / 2.0)).as(f64);
-
-			af::array Samp_Re = af::cos(Arg_phi);   // generation of the real part of the signal with 1 magnitude!
-			af::array Samp_Im = af::sin(Arg_phi);	// generation of the image part of the signal with 1 magnitude!
-
-			af::array S = af::complex(Samp_Re, Samp_Im);	//  creates complex LFM signal
-
-			// Padding the signal with zeros at the beginning and the end:
-			double Beg_pad = floor((Tr - prm.Ti - prm.Blank)*prm.Fs);
-			double End_pad = floor(((prm.Q - 1)*prm.Ti + prm.Blank - Tr)*prm.Fs);
-
-			af::array ZZbeg = af::constant(0., Beg_pad, c64);
-			af::array ZZend = af::constant(0., End_pad, c64);
-
-			S = af::join(0, ZZbeg, S);
-			S = af::join(0, S, ZZend);
-
-			return S;
-		}
-		else
-		{
-			double T_input = prm.Q*prm.Ti + prm.Blank - Tr;	// rec signal duration
-			double N = floor(T_input*prm.Fs);				// number of signal samples
-			af::array t(af::seq(N) / prm.Fs);	// creates an array of t [0:N-1]/fdis
-
-												// Phase of the input signal:
-			af::array Arg_phi = af::array(2.0*Pi*(prm.f0*t + mu*pow(t, 2.0) / 2.0)).as(f64);
-
-			af::array Samp_Re = af::cos(Arg_phi);   // generation of the real part of the signal with 1 magnitude!
-			af::array Samp_Im = af::sin(Arg_phi);	// generation of the image part of the signal with 1 magnitude!
-
-			af::array S = af::complex(Samp_Re, Samp_Im);	//  creates complex LFM signal
-
-			double Beg_pad = floor((Tr - prm.Ti - prm.Blank)*prm.Fs);
-			af::array ZZbeg = af::constant(0., Beg_pad, c64);
-			S = af::join(0, ZZbeg, S);
-
-			return S;
-		}
+		// Phase of the rec signal:
+		Arg_phi = af::array(2.0*Pi*(prm.f0*(k*t + T_shift) + mu*pow((k*t + T_shift), 2.0) / 2.0)).as(f64);
 	}
-	else if (prm.Sig == SigType::PSK)
-	{
+		break;
+	
+	case SigType::PSK:
 		// Returning void af::array object - has not been developed yet
 		return af::array();
+		break;
+
+	default:
+		std::cerr << "Invalid type of the signal!\n";
+		break;
 	}
-	else std::cerr << "Invalid type of the signal!\n";
+		
+	af::array Samp_Re = af::cos(Arg_phi);   // generation of the real part of the signal with 1 magnitude!
+	af::array Samp_Im = af::sin(Arg_phi);	// generation of the image part of the signal with 1 magnitude!
+
+	af::array S = af::complex(Samp_Re, Samp_Im);	//  creates complex LFM signal
+
+	S = af::join(0, ZZbeg, S);
+	S = af::join(0, S, ZZend);
+
+	return S;
 }
 
 #endif
